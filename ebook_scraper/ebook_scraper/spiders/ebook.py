6ee6a9d6c41cb118db5ae1274227f23c40f2bd9f
JIB2017 <blacuttjuan07@gmail.com>
2025-06-25 01:30:13 -0300

    Initial data extration

---
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class EbookSpider(scrapy.Spider):
+    name = "ebook"
+    start_urls = [ "https://books.toscrape.com/" ]
+
+    def parse(self, response):
+        ebooks = response.css("article")
+
+        for ebook in ebooks:
+            title = ebook.css("h3 a::text").get()
+            price = ebook.css("p.price_color::text").get()
+
+            print(title, price)
