20f2dfa1e260acedb979863893e839bdfeb18987
JIB2017 <blacuttjuan07@gmail.com>
2025-07-21 06:02:01 -0300

    Added pagination feature

--- ebook_scraper/ebook_scraper/spiders/ebook.py
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -4,8 +4,27 @@ from scrapy.loader import ItemLoader
 
 class EbookSpider(scrapy.Spider):
     name = "ebook"
-    start_urls = [ "https://books.toscrape.com/" ]
+    start_urls = [ 
+        "https://books.toscrape.com/", 
+        "https://books.toscrape.com/catalogue/category/books/mystery_3", 
+        "https://books.toscrape.com/catalogue/category/books/sequential-art_5"
+        ]
     cols = ["Title", "Price"]
+
+    def __init__(self):
+        super().__init__()
+        self.page_count = 1
+        self.page_total = 4
+
+    def start(self):
+        base_url = self.start_urls[2]
+        # Pagination
+        while self.page_count <= self.page_total:
+            yield scrapy.Request(
+                f"{base_url}/page-{self.page_count}.html"
+            )
+            self.page_count += 1
+        # return super().start()
 
     def parse(self, response):
         ebooks = response.css("article")
@@ -23,3 +42,11 @@ class EbookSpider(scrapy.Spider):
 
             # print(title, price)
             yield loader.load_item()
+        
+        # has_next_page = response.css("li.next a")
+
+        # if (has_next_page):
+        #     next_url = f"{self.start_urls[1]}/{has_next_page.attrib['href']}"
+        #     yield scrapy.Request(next_url)
+
+        
